4a18e60d1407f6645aee7899d3d257755c661c18
sheff4uk <sheff4uk@gmail.com>
2020-08-06 23:09:10 +0300

    fix

--- opening.php
+++ opening.php
@@ -233,7 +233,7 @@ $query = "
 $res = mysqli_query( $mysqli, $query ) or die("Invalid query: " .mysqli_error( $mysqli ));
 while( $row = mysqli_fetch_array($res) ) {
 	if( $row["LP_ID"] ) {
-		$cassette = "<a href='packing.php?date_from={$row["p_date"]}&date_to={$row["p_date"]}#{$row["LP_ID"]}' title='Упаковка' target='_blank'><b class='cassette'>{$row["cassette"]}</b></a>";
+		$cassette = "<a href='packing.php?p_date_from={$row["p_date"]}&p_date_to={$row["p_date"]}#{$row["LP_ID"]}' title='Упаковка' target='_blank'><b class='cassette'>{$row["cassette"]}</b></a>";
 	}
 	else {
 		$cassette = "<b class='cassette'>{$row["cassette"]}</b>";
