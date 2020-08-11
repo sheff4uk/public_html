9d38a4e3e8cc271eb771b2545cb569365b7df299
sheff4uk <sheff4uk@gmail.com>
2020-08-11 09:09:18 +0300

    test

--- forms/opening_form.php
+++ forms/opening_form.php
@@ -143,11 +143,11 @@ this.subbut.value='Подождите, пожалуйста!';">
 
 <script>
 	$(function() {
-		$('#opening_form select[name="LF_ID"]').select2({ placeholder: 'Выберите заливку', language: 'ru' });
+//		$('#opening_form select[name="LF_ID"]').select2({ placeholder: 'Выберите заливку', language: 'ru' });
 		// Костыль для Select2 чтобы работал поиск
-		$.ui.dialog.prototype._allowInteraction = function (e) {
-			return true;
-		};
+//		$.ui.dialog.prototype._allowInteraction = function (e) {
+//			return true;
+//		};
 
 //		<?
 //		if( isset($_GET["add"]) ) {
