1a0f67184fe95387feea5a61dbc7805345e1c34e
sheff4uk <sheff4uk@gmail.com>
2020-09-22 06:46:27 +0300

    fix

--- forms/plan_scheduling_form.php
+++ forms/plan_scheduling_form.php
@@ -98,7 +98,7 @@ this.subbut.value='Подождите, пожалуйста!';">
 								?>
 							</select>
 						</td>
-						<td><input type="number" name="pallets" min="0" max="30" style="width: 70px;" required></td>
+						<td><input type="number" name="pallets" min="0" max="50" style="width: 70px;" required></td>
 						<td><input type="number" name="amount" style="width: 70px;" readonly></td>
 					</tr>
 				</tbody>
