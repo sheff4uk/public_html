c09a121735f686fa8f00246d8eecb742ef357e6b
sheff4uk <sheff4uk@gmail.com>
2020-10-08 17:33:03 +0300

    test

--- cron/cubetest_SMS.php
+++ cron/cubetest_SMS.php
@@ -1,11 +1,11 @@
 #!/usr/bin/php
 <?
-$path = dirname($argv[0], 2);
+//$path = dirname($argv[0], 2);
 $key = $argv[1];
 $mtel = $argv[2];
 echo $argv[0];
-echo $path."/config.php";
-include $path."/config.php";
+//echo $path."/config.php";
+//include $path."/config.php";
 // Проверка доступа
 if( $key != $script_key ) die('Access denied!');
 
