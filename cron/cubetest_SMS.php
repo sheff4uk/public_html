0936dcda57c32e979cfb72ad422084486f36f51d
sheff4uk <sheff4uk@gmail.com>
2020-10-08 17:35:04 +0300

    test

--- cron/cubetest_SMS.php
+++ cron/cubetest_SMS.php
@@ -4,7 +4,7 @@
 $key = $argv[1];
 $mtel = $argv[2];
 echo $argv[0];
-echo dirname($argv[0], 2);
+echo dirname($argv[0]);
 //echo $path."/config.php";
 //include $path."/config.php";
 // Проверка доступа
