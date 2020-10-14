4bbf12bd4b21ca72ec4995fba3618c7638809897
sheff4uk <sheff4uk@gmail.com>
2020-10-14 03:25:42 +0300

    Версионность рецептов

--- mix_formula.php
+++ mix_formula.php
@@ -11,8 +11,9 @@ include "./forms/mix_formula_form.php";
 			<th rowspan="2">Противовес</th>
 			<th rowspan="2">Литера</th>
 			<th colspan="3">Условия применения</th>
+			<th rowspan="2" colspan="2">Версия</th>
 			<th colspan="5">Рецепт</th>
-			<th rowspan="2"></th>
+<!--			<th rowspan="2"></th>-->
 		</tr>
 		<tr>
 			<th>Окалина между</th>
@@ -40,18 +41,21 @@ $query = "
 		,MF.sn_max
 		,MF.cs_min
 		,MF.cs_max
-		,MF.iron_oxide
-		,MF.sand
-		,MF.crushed_stone
-		,MF.cement
-		,MF.water
+		,GROUP_CONCAT(CONCAT('<b>', MFV.version, '</b> (с ', DATE_FORMAT(MFV.from, '%d.%m.%Y %T'), ' по ', DATE_FORMAT(MFV.to, '%d.%m.%Y %T'), ')') SEPARATOR '<br>') version
+		,GROUP_CONCAT(MFV.iron_oxide SEPARATOR '<br>') iron_oxide
+		,GROUP_CONCAT(MFV.sand SEPARATOR '<br>') sand
+		,GROUP_CONCAT(MFV.crushed_stone SEPARATOR '<br>') crushed_stone
+		,GROUP_CONCAT(MFV.cement SEPARATOR '<br>') cement
+		,GROUP_CONCAT(MFV.water SEPARATOR '<br>') water
 	FROM MixFormula MF
+	JOIN MixFormulaVersions MFV ON MFV.MF_ID = MF.MF_ID
 	JOIN CounterWeight CW ON CW.CW_ID = MF.CW_ID
 	JOIN (
 		SELECT CW_ID, SUM(1) cnt
 		FROM MixFormula
 		GROUP BY CW_ID
 	) SUB ON SUB.CW_ID = MF.CW_ID
+	GROUP BY MFV.MF_ID
 	ORDER BY MF.CW_ID, MF.letter
 ";
 $res = mysqli_query( $mysqli, $query ) or die("Invalid query: " .mysqli_error( $mysqli ));
@@ -59,7 +63,7 @@ while( $row = mysqli_fetch_array($res) ) {
 	// Выводим общую ячейку с кодом противовеса
 	if( $CW_ID != $row["CW_ID"] ) {
 		echo "<tr id='{$row["MF_ID"]}' style='border-top: 2px solid #333;'>";
-		echo "<td rowspan='{$row["cnt"]}'>{$row["item"]}</td>";
+		echo "<td rowspan='{$row["cnt"]}'><b>{$row["item"]}</b></td>";
 		$cnt = 0;
 	}
 	else {
@@ -71,12 +75,13 @@ while( $row = mysqli_fetch_array($res) ) {
 		<td class="bg-gray"><?=(($row["io_min"] and $row["io_max"]) ? ($row["io_min"]/1000)." - ".($row["io_max"]/1000) : "")?></td>
 		<td class="bg-gray"><?=(($row["sn_min"] and $row["sn_max"]) ? ($row["sn_min"]/1000)." - ".($row["sn_max"]/1000) : "")?></td>
 		<td class="bg-gray"><?=(($row["cs_min"] and $row["cs_max"]) ? ($row["cs_min"]/1000)." - ".($row["cs_max"]/1000) : "")?></td>
+		<td colspan="2" class="nowrap"><span><?=$row["version"]?></span></td>
 		<td><?=$row["iron_oxide"]?></td>
 		<td><?=$row["sand"]?></td>
 		<td><?=$row["crushed_stone"]?></td>
 		<td><?=$row["cement"]?></td>
 		<td><?=$row["water"]?></td>
-		<td><a href="#" class="add_formula" MF_ID="<?=$row["MF_ID"]?>" title="Изменить рецепт"><i class="fa fa-pencil-alt fa-lg"></i></a></td>
+<!--		<td><a href="#" class="add_formula" MF_ID="<?=$row["MF_ID"]?>" title="Изменить рецепт"><i class="fa fa-pencil-alt fa-lg"></i></a></td>-->
 	</tr>
 	<?
 }
@@ -85,7 +90,7 @@ while( $row = mysqli_fetch_array($res) ) {
 	</tbody>
 </table>
 
-<div id="add_btn" class="add_formula" title="Добавить новый рецепт"></div>
+<!--<div id="add_btn" class="add_formula" title="Добавить новый рецепт"></div>-->
 
 <?
 include "footer.php";
