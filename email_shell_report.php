58ea4661273a45d8c79bb85cd2296c8ed9c800d5
sheff4uk <sheff4uk@gmail.com>
2020-12-23 07:07:58 +0300

    fix

--- email_shell_report.php
+++ email_shell_report.php
@@ -8,7 +8,7 @@ $sr_date_format = date_format($date, 'd/m/Y');
 $subject = "Shells report on {$sr_date_format}";
 
 $message = "
-	<table cellspacing='0' cellpadding='2' border='1'>
+	<table cellspacing='0' cellpadding='2' border='1' style='table-layout: fixed; width: 100%;'>
 		<tr>
 			<th><img src='https://konstanta.ltd/assets/images/logo.png' alt='KONSTANTA' style='width: 200px; margin: 5px;'></th>
 			<th>Report date: <n style='font-size: 2em;'>{$sr_date_format}</n></th>
