669940b901bc4a2290ec2992e7456e416087ede5
sheff4uk <sheff4uk@gmail.com>
2020-12-23 06:48:52 +0300

    test

--- test_mail.php
+++ test_mail.php
@@ -5,8 +5,8 @@ $subject = "Заголовок письма";
 
 $message = ' <p>Текст письма</p> </br> <b>1-ая строчка </b> </br><i>2-ая строчка </i> </br>';
 
-$headers  = "Content-type: text/html; charset=windows-1251 \r\n";
-$headers .= "From: planner (KONSTANTA) <planner@konstanta.ltd>\r\n";
+$headers  = "Content-type: text/html; charset=UTF-8 \r\n";
+$headers .= "From: planner_(KONSTANTA) <planner@konstanta.ltd>\r\n";
 
 mail($to, $subject, $message, $headers);
 ?>
