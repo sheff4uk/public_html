fef73b01d1588b3be3d75ae9b8475bd074037e00
sheff4uk <sheff4uk@gmail.com>
2020-12-23 07:00:14 +0300

    fix

--- test_mail.php
+++ test_mail.php
@@ -6,7 +6,7 @@ $subject = "Заголовок письма";
 $message = ' <p>Текст письма</p> </br> <b>1-ая строчка </b> </br><i>2-ая строчка </i> </br>';
 
 $headers  = "Content-type: text/html; charset=utf-8 \r\n";
-$headers .= "From: planner@konstanta.ltd\r\n";
+$headers .= "From: kis@konstanta.ltd\r\n";
 
 mail($to, $subject, $message, $headers);
 ?>
