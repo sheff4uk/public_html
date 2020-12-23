0925f3a6a77f168a7b656e6bd80610f505046e6f
sheff4uk <sheff4uk@gmail.com>
2020-12-23 06:41:36 +0300

    test php mail

---
+++ test_mail.php
@@ -0,0 +1,12 @@
+<?
+$to  = "sheff4uk@gmaail.com" ;
+
+$subject = "Заголовок письма";
+
+$message = ' <p>Текст письма</p> </br> <b>1-ая строчка </b> </br><i>2-ая строчка </i> </br>';
+
+$headers  = "Content-type: text/html; charset=windows-1251 \r\n";
+$headers .= "From: От кого письмо <planner@konstanta.ltd>\r\n";
+
+mail($to, $subject, $message, $headers);
+?>
