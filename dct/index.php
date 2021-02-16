9c65e7615ea844fd756a0ecae6620ec273a21d85
sheff4uk <sheff4uk@gmail.com>
2021-02-17 00:16:04 +0300

    test

--- dct/index.php
+++ dct/index.php
@@ -9,31 +9,25 @@
 			$(function() {
 				// Считывание штрихкода
 				var barcode="";
-				var codes="";
 				$(document).keydown(function(e)
 				{
 					var code = (e.keyCode ? e.keyCode : e.which);
-					if (code==0) alert('!!!');
+					if (code==0) barcode="";
 					if( code==13 || code==9 )// Enter key hit. Tab key hit.
 					{
 						//console.log(barcode);
-						alert(barcode.length);
 						alert(barcode);
-						alert(codes);
 						if( barcode.length == 8 ) {
 							//filling_form(Number(barcode));
 							barcode="";
-							codes="";
 							return false;
 						}
 						barcode="";
-						codes="";
 					}
 					else
 					{
 						if (code >= 48 && code <= 57) {
 							barcode = barcode + String.fromCharCode(code);
-							codes = codes + code + ',';
 						}
 					}
 				});
