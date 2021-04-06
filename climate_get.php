addf711847e27818b2fdc91c1c2162c60de6f864
sheff4uk <sheff4uk@gmail.com>
2021-04-06 16:18:16 +0300

    Проверка ip адреса

--- climate_get.php
+++ climate_get.php
@@ -7,17 +7,15 @@ $ip = $_SERVER['REMOTE_ADDR'];
 include "config.php";
 
 // Проверка доступа
-//if( !(strpos($ip, "92.255") === 0) ) die('Access denied!');
-
-
-// Записываем в базу переданные показания
-$query = "
-	INSERT INTO Climate
-	SET t1 = IF({$t1} != 0 AND {$h1} != 0, {$t1}, NULL)
-		,h1 = IF({$t1} != 0 AND {$h1} != 0, {$h1}, NULL)
-		,t2 = IF({$t2} != 0 AND {$h2} != 0, {$t2}, NULL)
-		,h2 = IF({$t2} != 0 AND {$h2} != 0, {$h2}, NULL)
-		,ip = '{$ip}'
-";
-mysqli_query( $mysqli, $query ) or die("Invalid query: " .mysqli_error( $mysqli ));
+if( $ip == "91.144.175.13" ) {
+	// Записываем в базу переданные показания
+	$query = "
+		INSERT INTO Climate
+		SET t1 = IF({$t1} != 0 AND {$h1} != 0, {$t1}, NULL)
+			,h1 = IF({$t1} != 0 AND {$h1} != 0, {$h1}, NULL)
+			,t2 = IF({$t2} != 0 AND {$h2} != 0, {$t2}, NULL)
+			,h2 = IF({$t2} != 0 AND {$h2} != 0, {$h2}, NULL)
+	";
+	mysqli_query( $mysqli, $query ) or die("Invalid query: " .mysqli_error( $mysqli ));
+}
 ?>
