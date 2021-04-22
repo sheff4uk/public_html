487ad8508f8d7c411a7ea8e04ac533f84604efff
sheff4uk <sheff4uk@gmail.com>
2021-04-22 10:27:43 +0300

    Запись в базу номеров кассет

--- barcode_get.php
+++ barcode_get.php
@@ -6,17 +6,35 @@ include "config.php";
 // Проверка доступа
 if( $ip == "91.144.175.13" ) {
 	// Узнаем префикс штрихкода
-	$prefix = substr($bc, 0, 1);
-	// Для префикса форм
-	if( $prefix == "2" ) {
-		$SI_ID = (int)substr($bc, 1);
-		echo $SI_ID;
-		// Записываем в базу переданные данные
-		$query = "
-			INSERT INTO shell__Log
-			SET SI_ID = {$SI_ID}
-		";
-		mysqli_query( $mysqli, $query ) or die("Invalid query: " .mysqli_error( $mysqli ));
+	$prefix1 = substr($bc, 0, 1);
+	$prefix2 = substr($bc, 0, 2);
+
+	switch( $prefix1 ) {
+		case "1":
+			switch( $prefix2 ) {
+				case "11":
+					$cassette = (int)substr($bc, 2);
+					// Записываем в базу переданные данные
+					$query = "
+						INSERT INTO cassette__Opening
+						SET cassette = {$cassette}
+					";
+					mysqli_query( $mysqli, $query ) or die("Invalid query: " .mysqli_error( $mysqli ));
+					break;
+				/////////////////////////////////////
+			}
+			break;
+		/////////////////////////////////////
+		case "2": // Формы
+			$SI_ID = (int)substr($bc, 1);
+			// Записываем в базу переданные данные
+			$query = "
+				INSERT INTO shell__Log
+				SET SI_ID = {$SI_ID}
+			";
+			mysqli_query( $mysqli, $query ) or die("Invalid query: " .mysqli_error( $mysqli ));
+			break;
+		/////////////////////////////////////
 	}
 }
 ?>
