f6c122f548acfc5b75272fabc080fb2e237a092e
sheff4uk <sheff4uk@gmail.com>
2021-04-25 09:19:46 +0300

    Защита от повторного сканирования

--- barcode_get.php
+++ barcode_get.php
@@ -14,12 +14,31 @@ if( $ip == "91.144.175.13" ) {
 			switch( $prefix2 ) {
 				case "11":
 					$cassette = (int)substr($bc, 2);
-					// Записываем в базу переданные данные
+					//Проверяем была ли эта кассета уже просканирован в течении часа
 					$query = "
-						INSERT INTO cassette__Opening
-						SET cassette = {$cassette}
+						SELECT event_time
+						FROM cassette__Opening
+						WHERE cassette = {$cassette} AND event_time BETWEEN (NOW() - INTERVAL 1 HOUR) AND NOW()
 					";
-					mysqli_query( $mysqli, $query ) or die("Invalid query: " .mysqli_error( $mysqli ));
+					$res = mysqli_query( $mysqli, $query ) or die("Invalid query: " .mysqli_error( $mysqli ));
+					// Если была кассета, то обновляем время
+					if( mysqli_num_rows($res) ) {
+						$row = mysqli_fetch_array($res);
+						$query = "
+							UPDATE cassette__Opening
+							SET event_time = NOW()
+							WHERE event_time = {$row["event_time"]} AND cassette = {$cassette}
+						";
+						mysqli_query( $mysqli, $query ) or die("Invalid query: " .mysqli_error( $mysqli ));
+					}
+					// Иначе добавляем кассету
+					else {
+						$query = "
+							INSERT INTO cassette__Opening
+							SET cassette = {$cassette}
+						";
+						mysqli_query( $mysqli, $query ) or die("Invalid query: " .mysqli_error( $mysqli ));
+					}
 					break;
 				/////////////////////////////////////
 			}
@@ -27,12 +46,21 @@ if( $ip == "91.144.175.13" ) {
 		/////////////////////////////////////
 		case "2": // Формы
 			$SI_ID = (int)substr($bc, 1);
-			// Записываем в базу переданные данные
+			//Проверяем была ли форма уже просканирован в течении часа
 			$query = "
-				INSERT INTO shell__Log
-				SET SI_ID = {$SI_ID}
+				SELECT event_time
+				FROM shell__Log
+				WHERE SI_ID = {$SI_ID} AND event_time BETWEEN (NOW() - INTERVAL 1 HOUR) AND NOW()
 			";
-			mysqli_query( $mysqli, $query ) or die("Invalid query: " .mysqli_error( $mysqli ));
+			$res = mysqli_query( $mysqli, $query ) or die("Invalid query: " .mysqli_error( $mysqli ));
+			// Если формы не было, записываем её в базу
+			if( mysqli_num_rows($res) == 0 ) {
+				$query = "
+					INSERT INTO shell__Log
+					SET SI_ID = {$SI_ID}
+				";
+				mysqli_query( $mysqli, $query ) or die("Invalid query: " .mysqli_error( $mysqli ));
+			}
 			break;
 		/////////////////////////////////////
 	}
