0c763adddf175159b1deaccf3d4531f4abdc7560
sheff4uk <sheff4uk@gmail.com>
2021-06-07 14:09:12 +0300

    Отклонения веса в списке регистраций деталей

--- ajax/opening_form_ajax.php
+++ ajax/opening_form_ajax.php
@@ -19,6 +19,7 @@ $html .= "
 
 $query = "
 	SELECT LW.weight
+		,IF(LW.weight BETWEEN ROUND(CW.min_weight * 1.02) AND ROUND(CW.max_weight * 1.02), 0, IF(LW.weight > ROUND(CW.max_weight * 1.02), LW.weight - ROUND(CW.max_weight * 1.02), LW.weight - ROUND(CW.min_weight * 1.02))) weight_diff
 		,LW.nextID
 		,DATE_FORMAT(LW.weighing_time, '%H:%i:%s') weighing_time_format
 		,LW.goodsID
@@ -26,6 +27,11 @@ $query = "
 		,WT.post
 	FROM list__Weight LW
 	LEFT JOIN WeighingTerminal WT ON WT.WT_ID = LW.WT_ID
+	LEFT JOIN list__Opening LO ON LO.LO_ID = LW.LO_ID
+	LEFT JOIN list__Filling LF ON LF.LF_ID = LO.LF_ID
+	LEFT JOIN list__Batch LB ON LB.LB_ID = LF.LB_ID
+	LEFT JOIN plan__Batch PB ON PB.PB_ID = LB.PB_ID
+	LEFT JOIN CounterWeight CW ON CW.CW_ID = PB.CW_ID
 	WHERE LW.LO_ID = {$LO_ID}
 	ORDER BY weighing_time
 ";
@@ -57,7 +63,7 @@ while( $row = mysqli_fetch_array($res) ) {
 
 	$html .= "
 		<tr>
-			<td>".($row["weight"]/1000)."</td>
+			<td>".($row["weight"]/1000).($row["weight_diff"] ? "<font style='font-size: .8em; display: block; line-height: .4em;' color='red'>".($row["weight_diff"] > 0 ? " +" : " ").($row["weight_diff"]/1000)."</font>" : "")."</td>
 			<td>{$row["weighing_time_format"]}</td>
 			<td>{$defect}</td>
 			<td>{$row["RN"]}</td>
