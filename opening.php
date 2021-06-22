90b7c4c9c64b14ce00bd987295b4ebbdf331db67
sheff4uk <sheff4uk@gmail.com>
2021-06-22 23:05:52 +0300

    Только средний вес

--- opening.php
+++ opening.php
@@ -261,7 +261,7 @@ foreach ($_GET as &$value) {
 			<th rowspan="2"><i class="far fa-lg fa-hourglass" title="Интервал в часах с моента заливки."></i></th>
 			<th colspan="2" rowspan="2">Брак</th>
 			<th rowspan="2">Деталей залито/зарегистрировано</th>
-			<th colspan="3">Взвешивания, кг</th>
+			<th rowspan="2">Средний вес, кг</th>
 			<th rowspan="2">Куб раствора, кг</th>
 			<th rowspan="2">Противовес</th>
 			<th rowspan="2">Дата заливки</th>
@@ -270,9 +270,6 @@ foreach ($_GET as &$value) {
 		<tr>
 			<th>Дата</th>
 			<th>Время</th>
-			<th>MIN</th>
-			<th>AVG</th>
-			<th>MAX</th>
 		</tr>
 	</thead>
 	<tbody style="text-align: center;">
@@ -291,9 +288,9 @@ $query = "
 		,LOD.def_assembly
 		,PB.in_cassette
 		,COUNT(DISTINCT LW.LW_ID) cnt_weight
-		,MIN(LW.weight) min_weight
+		#,MIN(LW.weight) min_weight
 		,ROUND(AVG(LW.weight)) avg_weight
-		,MAX(LW.weight) max_weight
+		#,MAX(LW.weight) max_weight
 		#,IF(MIN(LW.weight) BETWEEN ROUND(CW.min_weight + (CW.min_weight/100*CW.drying_percent)) AND ROUND(CW.max_weight + (CW.max_weight/100*CW.drying_percent)), 0, IF(MIN(LW.weight) > ROUND(CW.max_weight + (CW.max_weight/100*CW.drying_percent)), MIN(LW.weight) - ROUND(CW.max_weight + (CW.max_weight/100*CW.drying_percent)), MIN(LW.weight) - ROUND(CW.min_weight + (CW.min_weight/100*CW.drying_percent)))) min_diff
 		#,IF(ROUND(AVG(LW.weight)) BETWEEN ROUND(CW.min_weight + (CW.min_weight/100*CW.drying_percent)) AND ROUND(CW.max_weight + (CW.max_weight/100*CW.drying_percent)), 0, IF(ROUND(AVG(LW.weight)) > ROUND(CW.max_weight + (CW.max_weight/100*CW.drying_percent)), ROUND(AVG(LW.weight)) - ROUND(CW.max_weight + (CW.max_weight/100*CW.drying_percent)), ROUND(AVG(LW.weight)) - ROUND(CW.min_weight + (CW.min_weight/100*CW.drying_percent)))) avg_diff
 		#,IF(MAX(LW.weight) BETWEEN ROUND(CW.min_weight + (CW.min_weight/100*CW.drying_percent)) AND ROUND(CW.max_weight + (CW.max_weight/100*CW.drying_percent)), 0, IF(MAX(LW.weight) > ROUND(CW.max_weight + (CW.max_weight/100*CW.drying_percent)), MAX(LW.weight) - ROUND(CW.max_weight + (CW.max_weight/100*CW.drying_percent)), MAX(LW.weight) - ROUND(CW.min_weight + (CW.min_weight/100*CW.drying_percent)))) max_diff
@@ -360,9 +357,9 @@ while( $row = mysqli_fetch_array($res) ) {
 			<?=($row["def_assembly"] ? "<font color='red'>{$row["def_assembly"]}</font> дефект сборки<br>" : "")?>
 		</td>
 		<td <?=(abs($row["in_cassette"] - $row["cnt_weight"]) > $row["in_cassette"] / 2 ? "style='color: red;'" : "")?>><?=$row["in_cassette"]?> / <?=$row["cnt_weight"]?></td>
-		<td><?=($row["min_weight"] ? $row["min_weight"]/1000 : "")?><?=($row["min_diff"] ? "<font style='font-size: .8em; display: block; line-height: .4em;' color='red'>".($row["min_diff"] > 0 ? " +" : " ").($row["min_diff"]/1000)."</font>" : "")?></td>
+<!--		<td><?=($row["min_weight"] ? $row["min_weight"]/1000 : "")?><?=($row["min_diff"] ? "<font style='font-size: .8em; display: block; line-height: .4em;' color='red'>".($row["min_diff"] > 0 ? " +" : " ").($row["min_diff"]/1000)."</font>" : "")?></td>-->
 		<td><a href="#" class="edit_transactions" LO_ID="<?=$row["LO_ID"]?>" title="Список регистраций"><?=($row["avg_weight"] ? $row["avg_weight"]/1000 : "")?><?=($row["avg_diff"] ? "<font style='font-size: .8em; display: block; line-height: .4em;' color='red'>".($row["avg_diff"] > 0 ? " +" : " ").($row["avg_diff"]/1000)."</font>" : "")?></a></td>
-		<td><?=($row["max_weight"] ? $row["max_weight"]/1000 : "")?><?=($row["max_diff"] ? "<font style='font-size: .8em; display: block; line-height: .4em;' color='red'>".($row["max_diff"] > 0 ? " +" : " ").($row["max_diff"]/1000)."</font>" : "")?></td>
+<!--		<td><?=($row["max_weight"] ? $row["max_weight"]/1000 : "")?><?=($row["max_diff"] ? "<font style='font-size: .8em; display: block; line-height: .4em;' color='red'>".($row["max_diff"] > 0 ? " +" : " ").($row["max_diff"]/1000)."</font>" : "")?></td>-->
 		<td class="bg-gray"><?=$row["mix_density"]/1000?><?=($row["mix_diff"] ? "<font style='font-size: .8em; display: block; line-height: .4em;' color='red'>".($row["mix_diff"] > 0 ? " +" : " ").($row["mix_diff"]/1000)."</font>" : "")?></td>
 		<td class="bg-gray"><?=$row["item"]?></td>
 		<td class="bg-gray"><a href="filling.php?week=<?=$row["lb_week"]?>#<?=$row["LB_ID"]?>" title="Заливка" target="_blank"><?=$row["batch_date_format"]?></a></td>
