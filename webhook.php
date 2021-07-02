224627e0d41a85329d47cfbb8c75933a2cf835b3
sheff4uk <sheff4uk@gmail.com>
2021-07-02 15:56:21 +0300

    Вебхук для Телеграмм

---
+++ webhook.php
@@ -0,0 +1,19 @@
+<?
+include "config.php";
+
+$bot = new \TelegramBot\Api\Client(TELEGRAM_TOKEN);
+// команда для start
+$bot->command('start', function ($message) use ($bot) {
+	$answer = 'Добро пожаловать!';
+	$bot->sendMessage($message->getChat()->getId(), $answer);
+});
+
+// команда для помощи
+$bot->command('help', function ($message) use ($bot) {
+	$answer = 'Команды:
+/help - вывод справки';
+	$bot->sendMessage($message->getChat()->getId(), $answer);
+});
+
+$bot->run();
+?>
