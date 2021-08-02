feb75e2c1b6c31dd5c806a57b6cb95c06937cfa1
sheff4uk <sheff4uk@gmail.com>
2021-08-03 00:10:47 +0300

    fix

--- cron/email_production_report.php
+++ cron/email_production_report.php
@@ -162,6 +162,6 @@ $message .= "
 $headers  = "Content-type: text/html; charset=utf-8 \r\n";
 $headers .= "From: planner@konstanta.ltd\r\n";
 
-mail($to, $subject, $message, $headers);
-echo $message;
+//mail($to, $subject, $message, $headers);
+mail($to, $subject, "Hello!", $headers);
 ?>
