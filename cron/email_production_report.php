fac3802700d755e3887c4158d9ba4c83941a664f
sheff4uk <sheff4uk@gmail.com>
2021-08-03 00:18:43 +0300

    fix

--- cron/email_production_report.php
+++ cron/email_production_report.php
@@ -163,5 +163,5 @@ $headers  = "Content-type: text/html; charset=utf-8 \r\n";
 $headers .= "From: planner@konstanta.ltd\r\n";
 
 //mail($to, $subject, $message, $headers);
-mail("sheff4uk@gmail.com", $subject, "Hello!", $headers);
+mail($to, $subject, "Hello!", $headers);
 ?>
