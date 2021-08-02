8e027483f0e65441854a625af5625bfc1d499719
sheff4uk <sheff4uk@gmail.com>
2021-08-02 23:36:50 +0300

    fix

--- cron/email_production_report.php
+++ cron/email_production_report.php
@@ -54,7 +54,7 @@ $query = "
 	JOIN list__Weight LW ON LW.LO_ID = LO.LO_ID
 	#WHERE DATE(LO.opening_time - INTERVAL 7 HOUR) = CURDATE() - INTERVAL 1 DAY
 	GROUP BY LO.LO_ID
-	HAVING d_shell OR d_assembly
+	#HAVING d_shell OR d_assembly
 	ORDER BY LO.opening_time
 ";
 $res = mysqli_query( $mysqli, $query ) or die("Invalid query: " .mysqli_error( $mysqli ));
