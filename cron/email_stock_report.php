a40439ae19168732775ff95e8fa96853a51e774f
sheff4uk <sheff4uk@gmail.com>
2021-11-26 15:55:38 +0300

    fix

--- cron/email_stock_report.php
+++ cron/email_stock_report.php
@@ -60,5 +60,5 @@ $message .= "
 $headers = "Content-type: text/html; charset=\"utf-8\"\n";
 $headers .= "From: planner@konstanta.ltd\r\n";
 
-mail($to, $subject, '', $headers);
+mail($to, $subject, $message, $headers);
 ?>
