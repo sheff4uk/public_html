b32b6c5a22271850e476f19b9ac38d264904b5db
sheff4uk <sheff4uk@gmail.com>
2021-11-26 15:48:14 +0300

    fix

--- cron/email_stock_report.php
+++ cron/email_stock_report.php
@@ -9,8 +9,7 @@ if( $key != $script_key ) die('Access denied!');
 
 $date = date_create();
 $date_format = date_format($date, 'd.m.Y');
-//$subject = "=?utf-8?b?". base64_encode("[KONSTANTA_KRV] Складской остаток {$date_format}"). "?=";
-$subject = "=?utf-8?b?". base64_encode("[KONSTANTA_KRV] Stock report {$date_format}"). "?=";
+$subject = "=?utf-8?b?". base64_encode("[KONSTANTA] Складской остаток {$date_format}"). "?=";
 
 $message = "
 	<table cellspacing='0' cellpadding='2' border='1' style='table-layout: fixed; width: 100%;'>
@@ -58,7 +57,7 @@ $message .= "
 	</table>
 ";
 
-$headers  = "Content-type: text/html; charset=\"utf-8\"\n";
+$headers = "Content-type: text/html; charset=\"utf-8\"\n";
 $headers .= "From: planner@konstanta.ltd\r\n";
 
 mail($to, $subject, $message, $headers);
