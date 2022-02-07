ace212ac4d881df528b96d63945494082cf5417f
sheff4uk <sheff4uk@gmail.com>
2022-02-07 15:08:07 +0300

    Отклонения от рецепта видны

--- filling.php
+++ filling.php
@@ -260,14 +260,14 @@ while( $row = mysqli_fetch_array($res) ) {
 			,SUM(LF.underfilling) underfilling
 			,LB.test
 			,mix_diff({$row["CW_ID"]}, LB.mix_density) mix_diff
-			#,mix_sf_diff({$row["MF_ID"]}, LB.s_fraction) sf_diff
-			#,mix_lf_diff({$row["MF_ID"]}, LB.l_fraction) lf_diff
-			#,mix_io_diff({$row["MF_ID"]}, LB.iron_oxide) io_diff
-			#,mix_sn_diff({$row["MF_ID"]}, LB.sand) sn_diff
-			#,mix_cs_diff({$row["MF_ID"]}, LB.crushed_stone) cs_diff
-			#,mix_cm_diff({$row["MF_ID"]}, LB.cement) cm_diff
-			#,mix_pl_diff({$row["MF_ID"]}, LB.plasticizer) pl_diff
-			#,mix_wt_diff({$row["MF_ID"]}, LB.water) wt_diff
+			,mix_sf_diff({$row["MF_ID"]}, LB.s_fraction) sf_diff
+			,mix_lf_diff({$row["MF_ID"]}, LB.l_fraction) lf_diff
+			,mix_io_diff({$row["MF_ID"]}, LB.iron_oxide) io_diff
+			,mix_sn_diff({$row["MF_ID"]}, LB.sand) sn_diff
+			,mix_cs_diff({$row["MF_ID"]}, LB.crushed_stone) cs_diff
+			,mix_cm_diff({$row["MF_ID"]}, LB.cement) cm_diff
+			,mix_pl_diff({$row["MF_ID"]}, LB.plasticizer) pl_diff
+			,mix_wt_diff({$row["MF_ID"]}, LB.water) wt_diff
 		FROM plan__Batch PB
 		JOIN list__Batch LB ON LB.PB_ID = PB.PB_ID
 		LEFT JOIN list__Filling LF ON LF.LB_ID = LB.LB_ID
