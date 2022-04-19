9d8efb5f904a2c210c6876dd7c5210f051b6a4d4
sheff4uk <sheff4uk@gmail.com>
2022-04-19 23:04:15 +0300

    Уведомления по кубикам разделены по участкам.

--- cron/cubetest_SMS.php
+++ cron/cubetest_SMS.php
@@ -1,43 +1,53 @@
 <?
 $path = dirname(dirname($argv[0]));
 $key = $argv[1];
-//$mtel = $argv[2];
 include $path."/config.php";
 // Проверка доступа
 if( $key != $script_key ) die('Access denied!');
 
+// Цикл по производственным участкам
 $query = "
-	SELECT CW.item
-		,DATE_FORMAT(LB.batch_time, '%H:%i') time
-		,24 delay
-	FROM list__Batch LB
-	JOIN plan__Batch PB ON PB.PB_ID = LB.PB_ID
-	JOIN CounterWeight CW ON CW.CW_ID = PB.CW_ID
-	LEFT JOIN list__CubeTest LCT ON LCT.LB_ID = LB.LB_ID AND LCT.delay = 24
-	WHERE LB.test = 1
-		AND LCT.LCT_ID IS NULL
-		AND LB.batch_date + INTERVAL 1 DAY = CURDATE()
-		AND HOUR(LB.batch_time) = HOUR(CURTIME())
-	UNION ALL
-	SELECT CW.item
-		,DATE_FORMAT(LB.batch_time, '%H:%i') time
-		,72 delay
-	FROM list__Batch LB
-	JOIN plan__Batch PB ON PB.PB_ID = LB.PB_ID
-	JOIN CounterWeight CW ON CW.CW_ID = PB.CW_ID
-	LEFT JOIN list__CubeTest LCT ON LCT.LB_ID = LB.LB_ID AND LCT.delay = 72
-	WHERE LB.test = 1
-		AND LCT.LCT_ID IS NULL
-		AND LB.batch_date + INTERVAL 3 DAY = CURDATE()
-		AND HOUR(LB.batch_time) = HOUR(CURTIME())
-	ORDER BY time
+	SELECT F_ID
+		,cube_test_group
+	FROM factory
 ";
 $res = mysqli_query( $mysqli, $query ) or die("Invalid query: " .mysqli_error( $mysqli ));
 while( $row = mysqli_fetch_array($res) ) {
-	$text .= "<b>{$row["item"]}</b> {$row["time"]} [{$row["delay"]}]\n";
+	$query = "
+		SELECT CW.item
+			,DATE_FORMAT(LB.batch_time, '%H:%i') time
+			,24 delay
+		FROM list__Batch LB
+		JOIN plan__Batch PB ON PB.PB_ID = LB.PB_ID
+		JOIN CounterWeight CW ON CW.CW_ID = PB.CW_ID
+		LEFT JOIN list__CubeTest LCT ON LCT.LB_ID = LB.LB_ID AND LCT.delay = 24
+		WHERE LB.test = 1
+			AND PB.F_ID = {$row["F_ID"]}
+			AND LCT.LCT_ID IS NULL
+			AND LB.batch_date + INTERVAL 1 DAY = CURDATE()
+			AND HOUR(LB.batch_time) = HOUR(CURTIME())
+		UNION ALL
+		SELECT CW.item
+			,DATE_FORMAT(LB.batch_time, '%H:%i') time
+			,72 delay
+		FROM list__Batch LB
+		JOIN plan__Batch PB ON PB.PB_ID = LB.PB_ID
+		JOIN CounterWeight CW ON CW.CW_ID = PB.CW_ID
+		LEFT JOIN list__CubeTest LCT ON LCT.LB_ID = LB.LB_ID AND LCT.delay = 72
+		WHERE LB.test = 1
+			AND PB.F_ID = {$row["F_ID"]}
+			AND LCT.LCT_ID IS NULL
+			AND LB.batch_date + INTERVAL 3 DAY = CURDATE()
+			AND HOUR(LB.batch_time) = HOUR(CURTIME())
+		ORDER BY time
+	";
+	$subres = mysqli_query( $mysqli, $query ) or die("Invalid query: " .mysqli_error( $mysqli ));
+	while( $subrow = mysqli_fetch_array($subres) ) {
+		$text .= "<b>{$subrow["item"]}</b> {$subrow["time"]} [{$subrow["delay"]}]\n";
+	}
+	if( $text ) {
+		message_to_telegram($text, $row["cube_test_group"]);
+	}
 }
-if( $text ) {
-	//$body = file_get_contents("https://sms.ru/sms/send?api_id=".($api_id)."&to=".($mtel)."&msg=".urlencode($text)."&json=1");
-	message_to_telegram($text, '-1001582214873');
-}
+
 ?>
