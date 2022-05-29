a199797df26e5427ea6795c7d1b8d87d4f260da8
sheff4uk <sheff4uk@gmail.com>
2022-05-29 13:52:28 +0300

    test ip-api

--- login.php
+++ login.php
@@ -177,6 +177,15 @@ switch( $_GET["do"] ) {
 		</div>
 
 <?
+$ch = curl_init('http://ip-api.com/json/' . $_SERVER['REMOTE_ADDR'] . '?lang=ru');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_HEADER, false);
+$res = curl_exec($ch);
+curl_close($ch);
+
+$res = json_decode($res, true);
+print_r($res);
 	include "footer.php";
 }
 ?>
