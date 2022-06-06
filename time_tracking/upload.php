c0353d7252f79bf061df54a17f1ed93cf4e817c9
sheff4uk <sheff4uk@gmail.com>
2022-06-06 19:41:50 +0300

    fix

--- time_tracking/upload.php
+++ time_tracking/upload.php
@@ -1,18 +1,18 @@
 <?
 // new filename
 $filename = 'pic_'.date('YmdHis') . '.jpeg';
-	include_once "../config.php";
-	$query = "
-		UPDATE TimeTracking
-		SET ".($_GET["status"] ? "photo_start" : "photo_stop")." = {$filename}
-		WHERE TT_ID = {$_GET["tt_id"]}
-	";
-	mysqli_query( $mysqli, $query ) or die("Invalid query: " .mysqli_error( $mysqli ));
 
 $url = '';
 if( move_uploaded_file($_FILES['webcam']['tmp_name'],'upload/'.$filename) ){
 	$url = 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['REQUEST_URI']) . '/upload/' . $filename;
 
+	include_once "../config.php";
+	$query = "
+		UPDATE TimeTracking
+		SET ".($_GET["status"] ? "photo_start" : "photo_stop")." = '{$filename}'
+		WHERE TT_ID = {$_GET["tt_id"]}
+	";
+	mysqli_query( $mysqli, $query ) or die("Invalid query: " .mysqli_error( $mysqli ));
 }
 
 // Return image url
