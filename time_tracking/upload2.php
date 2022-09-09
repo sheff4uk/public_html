321bab222dc171a3820fa9a00a49a476a1ac8a49
sheff4uk <sheff4uk@gmail.com>
2022-09-09 17:40:23 +0300

    fix

--- time_tracking/upload2.php
+++ time_tracking/upload2.php
@@ -4,7 +4,7 @@ $filename = 'pic_'.date('YmdHis') . '.jpeg';
 
 $url = '';
 if( move_uploaded_file($_FILES['webcam']['tmp_name'],'upload/'.$filename) ){
-	$url = '/upload/' . $filename;
+	$url = dirname($_SERVER['REQUEST_URI']) . '/upload/' . $filename;
 
 //	include_once "../config.php";
 //	$query = "
