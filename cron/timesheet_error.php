adc73f6f1ec22dec6bed9dfb19f4d65b52fd085f
sheff4uk <sheff4uk@gmail.com>
2022-09-24 07:59:22 +0300

    Автоматически проставляются выходные в табеле

--- cron/timesheet_error.php
+++ cron/timesheet_error.php
@@ -13,6 +13,7 @@ $query = "
 ";
 $res = mysqli_query( $mysqli, $query ) or die("Invalid query: " .mysqli_error( $mysqli ));
 while( $row = mysqli_fetch_array($res) ) {
+	$F_ID = $row["F_ID"];
 	$query = "
 		SELECT DATE_FORMAT(CURDATE(), '%d.%m.%Y') date
 			,USR_Name(TS.USR_ID) name
@@ -20,7 +21,7 @@ while( $row = mysqli_fetch_array($res) ) {
 		FROM Timesheet TS
 		JOIN TimeReg TR ON TR.TS_ID = TS.TS_ID AND TR.del_time IS NULL
 		WHERE TS.ts_date = CURDATE()
-			AND TS.F_ID = {$row["F_ID"]}
+			AND TS.F_ID = {$F_ID}
 		GROUP BY TS.USR_ID
 		HAVING cnt = 1
 	";
@@ -31,6 +32,50 @@ while( $row = mysqli_fetch_array($res) ) {
 	}
 	if( $text ) {
 		message_to_telegram($text, $row["notification_group"]);
+	}
+
+	//////////////////////////////////////////////
+	// Автоматически помечаем выходные в табеле //
+	//////////////////////////////////////////////
+	$year = date('Y');
+	$month = date('m');
+	$xml = simplexml_load_file("http://xmlcalendar.ru/data/ru/".$year."/calendar.xml");
+	$json = json_encode($xml);
+	$data = json_decode($json,TRUE);
+	$day_of_week = date('N');	// День недели 1..7
+	$day = date('d');			// День месяца
+	// Перебираем массив и если находим дату то проверяем ее тип (тип дня: 1 - выходной день, 2 - рабочий и сокращенный (может быть использован для любого дня недели), 3 - рабочий день (суббота/воскресенье))
+	$t = 0;
+	foreach( $data["days"]["day"] as $key=>$value ) {
+		if( $value["@attributes"]["d"] == $month.".".$day) {
+			$t = $value["@attributes"]["t"];
+		}
+	}
+	// Если пришелся выходной день
+	if ( (($day_of_week >= 6 and $t != "3" and $t != "2") or ($t == "1")) ) {
+		// Цикл по работникам
+		$query = "
+			SELECT USR.USR_ID
+			FROM Users USR
+			JOIN TariffMonth TM ON TM.year = {$year}
+				AND TM.month = {$month}
+				AND TM.USR_ID = USR.USR_ID
+				AND TM.F_ID = {$F_ID}
+		";
+		$subres = mysqli_query( $mysqli, $query ) or die("Invalid query: " .mysqli_error( $mysqli ));
+		while( $subrow = mysqli_fetch_array($subres) ) {
+			// Проставляем выходные в табеле
+			$query = "
+				INSERT INTO Timesheet
+				SET ts_date = '{$year}-{$month}-{$day}'
+					,USR_ID = {$subrow["USR_ID"]}
+					,F_ID = {$F_ID}
+					,status = 4 #выходной
+				ON DUPLICATE KEY UPDATE
+					status = IF(duration IS NULL AND pay IS NULL AND status IS NULL, 4, status)
+			";
+			mysqli_query( $mysqli, $query ) or die("Invalid query: " .mysqli_error( $mysqli ));
+		}
 	}
 }
 
