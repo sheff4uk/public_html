6118a0a36f46249a54dadc52f60e313741682440
sheff4uk <sheff4uk@gmail.com>
2022-12-04 23:41:22 +0300

    fix

--- cron/timesheet_error.php
+++ cron/timesheet_error.php
@@ -64,29 +64,6 @@ while( $row = mysqli_fetch_array($res) ) {
 		message_to_telegram($text, $row["notification_group"]);
 	}
 
-
-
-
-//	$query = "
-//		SELECT DATE_FORMAT(CURDATE(), '%d.%m.%Y') date
-//			,USR_Name(TS.USR_ID) name
-//			,SUM(1) cnt
-//		FROM Timesheet TS
-//		JOIN TimeReg TR ON TR.TS_ID = TS.TS_ID AND TR.del_time IS NULL
-//		WHERE TS.ts_date = CURDATE()
-//			AND TS.F_ID = {$F_ID}
-//		GROUP BY TS.USR_ID
-//		HAVING cnt = 1
-//	";
-//	$subres = mysqli_query( $mysqli, $query ) or die("Invalid query: " .mysqli_error( $mysqli ));
-//	$text = "";
-//	while( $subrow = mysqli_fetch_array($subres) ) {
-//		$text .= "<b>Смена не закрыта!</b> {$subrow["name"]} ({$subrow["date"]})\n";
-//	}
-//	if( $text ) {
-//		message_to_telegram($text, $row["notification_group"]);
-//	}
-
 	//////////////////////////////////////////////
 	// Автоматически помечаем выходные в табеле //
 	//////////////////////////////////////////////
@@ -99,7 +76,7 @@ while( $row = mysqli_fetch_array($res) ) {
 			AND F_ID = {$F_ID}
 	";
 	$subres = mysqli_query( $mysqli, $query ) or die("Invalid query: " .mysqli_error( $mysqli ));
-	$subrow = mysqli_fetch_array($subres)
+	$subrow = mysqli_fetch_array($subres);
 	$shift_end = $subrow["shift_end"];
 
 	// Получаем время в минутах прошедшее с начала суток
@@ -123,7 +100,7 @@ while( $row = mysqli_fetch_array($res) ) {
 				,DAY(CURDATE() - INTERVAL {$interval_day} DAY) day
 		";
 		$subres = mysqli_query( $mysqli, $query ) or die("Invalid query: " .mysqli_error( $mysqli ));
-		$subrow = mysqli_fetch_array($subres)
+		$subrow = mysqli_fetch_array($subres);
 		$year = $subrow["year"];
 		$month = $subrow["month"];
 		$day_of_week = $subrow["day_of_week"];
