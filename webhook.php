39ce7033c64be33cafeae59c7c45d4175dd00775
sheff4uk <sheff4uk@gmail.com>
2022-12-07 08:17:58 +0300

    test

--- webhook.php
+++ webhook.php
@@ -1,11 +1,12 @@
 <?
 include "config.php";
 
-$data = json_decode(file_get_contents('php://input')); // получаем JSON
-$text = $data->{'message'}->{'text'};
+$json = file_get_contents('php://input');
+//$data = json_decode(file_get_contents('php://input')); // получаем JSON
+//$text = $data->{'message'}->{'text'};
 
 //$query = "INSERT INTO test SET command = '{$text}'";
-$query = "INSERT INTO test SET command = '{$data}'";
+$query = "INSERT INTO test SET command = '{$json}'";
 mysqli_query( $mysqli, $query ) or die("Invalid query: " .mysqli_error( $mysqli ));
 
 ?>
