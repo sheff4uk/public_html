b301cd0404adbb6daff94a18f0d7bbdb706502b0
sheff4uk <sheff4uk@gmail.com>
2023-03-01 12:41:02 +0300

    fix

--- cron/email_def_report.php
+++ cron/email_def_report.php
@@ -21,59 +21,60 @@ $month = $row["month"];
 $format_month = $row["format_month"];
 /////////////////////////////////////////
 
-$subject = "=?utf-8?b?". base64_encode("[KONSTANTA] Дефекты {$format_month} {$year}"). "?=";
+//$subject = "=?utf-8?b?". base64_encode("[KONSTANTA] Дефекты {$format_month} {$year}"). "?=";
+$subject = "=?utf-8?b?". base64_encode("[KONSTANTA] Monthly def report"). "?=";
 
 $message = "";
 
-//$message .= "
-//	<table cellspacing='0' cellpadding='2' border='1' style='table-layout: fixed; width: 100%;'>\r\n
-//		<tr>\r\n
-//			<th><img src='https://konstanta.ltd/assets/images/logo.png' alt='KONSTANTA' style='width: 200px; margin: 5px;'></th>\r\n
-//			<th><n style='font-size: 2em;'>Дефекты форм, сборки</n></th>\r\n
-//			<th>{$format_month} {$year}</th>\r\n
-//		</tr>\r\n
-//	</table>\r\n
-//
-//	<table cellspacing='0' cellpadding='2' border='1' style='table-layout: fixed; width: 100%;'>\r\n
-//		<thead style='word-wrap: break-word;'>\r\n
-//			<tr>\r\n
-//				<th>Мастер</th>\r\n
-//				<th>Дефект формы</th>\r\n
-//				<th>Дефект сборки</th>\r\n
-//			</tr>\r\n
-//		</thead>\r\n
-//		<tbody style='text-align: center;'>\r\n
-//";
-//
-//$query = "
-//	SELECT USR_Name(LA.assembling_master) `master`
-//		,CONCAT(SUM(IF(LW.goodsID = 6, 1, 0)), ' (', ROUND(SUM(IF(LW.goodsID = 6, 1, 0)) / SUM(1) * 100, 2), '%)') def_shell
-//		,CONCAT(SUM(IF(LW.goodsID = 7, 1, 0)), ' (', ROUND(SUM(IF(LW.goodsID = 7, 1, 0)) / SUM(1) * 100, 2), '%)') def_assembly
-//	FROM list__Assembling LA
-//	JOIN list__Filling LF ON LF.LA_ID = LA.LA_ID
-//	JOIN list__Opening LO ON LO.LF_ID = LF.LF_ID
-//	JOIN list__Weight LW ON LW.LO_ID = LO.LO_ID
-//	WHERE YEAR(LA.assembling_time) = {$year}
-//		AND MONTH(LA.assembling_time) = {$month}
-//		AND LA.assembling_master IS NOT NULL
-//	GROUP BY LA.assembling_master";
-//$res = mysqli_query( $mysqli, $query ) or die("Invalid query1: " .mysqli_error( $mysqli ));
-//while( $row = mysqli_fetch_array($res) ) {
-//	$message .= "
-//		<tr>\r\n
-//			<td>{$row["master"]}</td>\r\n
-//			<td>{$row["def_shell"]}</td>\r\n
-//			<td>{$row["def_assembly"]}</td>\r\n
-//		</tr>\r\n
-//	";
-//}
-//
-//$message .= "
-//		</tbody>\r\n
-//	</table>\r\n
-//	<br>\r\n
-//	<br>\r\n
-//";
+$message .= "
+	<table cellspacing='0' cellpadding='2' border='1' style='table-layout: fixed; width: 100%;'>\r\n
+		<tr>\r\n
+			<th><img src='https://konstanta.ltd/assets/images/logo.png' alt='KONSTANTA' style='width: 200px; margin: 5px;'></th>\r\n
+			<th><n style='font-size: 2em;'>Дефекты форм, сборки</n></th>\r\n
+			<th>{$format_month} {$year}</th>\r\n
+		</tr>\r\n
+	</table>\r\n
+
+	<table cellspacing='0' cellpadding='2' border='1' style='table-layout: fixed; width: 100%;'>\r\n
+		<thead style='word-wrap: break-word;'>\r\n
+			<tr>\r\n
+				<th>Мастер</th>\r\n
+				<th>Дефект формы</th>\r\n
+				<th>Дефект сборки</th>\r\n
+			</tr>\r\n
+		</thead>\r\n
+		<tbody style='text-align: center;'>\r\n
+";
+
+$query = "
+	SELECT USR_Name(LA.assembling_master) `master`
+		,CONCAT(SUM(IF(LW.goodsID = 6, 1, 0)), ' (', ROUND(SUM(IF(LW.goodsID = 6, 1, 0)) / SUM(1) * 100, 2), '%)') def_shell
+		,CONCAT(SUM(IF(LW.goodsID = 7, 1, 0)), ' (', ROUND(SUM(IF(LW.goodsID = 7, 1, 0)) / SUM(1) * 100, 2), '%)') def_assembly
+	FROM list__Assembling LA
+	JOIN list__Filling LF ON LF.LA_ID = LA.LA_ID
+	JOIN list__Opening LO ON LO.LF_ID = LF.LF_ID
+	JOIN list__Weight LW ON LW.LO_ID = LO.LO_ID
+	WHERE YEAR(LA.assembling_time) = {$year}
+		AND MONTH(LA.assembling_time) = {$month}
+		AND LA.assembling_master IS NOT NULL
+	GROUP BY LA.assembling_master";
+$res = mysqli_query( $mysqli, $query ) or die("Invalid query1: " .mysqli_error( $mysqli ));
+while( $row = mysqli_fetch_array($res) ) {
+	$message .= "
+		<tr>\r\n
+			<td>{$row["master"]}</td>\r\n
+			<td>{$row["def_shell"]}</td>\r\n
+			<td>{$row["def_assembly"]}</td>\r\n
+		</tr>\r\n
+	";
+}
+
+$message .= "
+		</tbody>\r\n
+	</table>\r\n
+	<br>\r\n
+	<br>\r\n
+";
 
 $message .= "
 	<table cellspacing='0' cellpadding='2' border='1' style='table-layout: fixed; width: 100%;'>\r\n
