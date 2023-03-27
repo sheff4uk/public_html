8e9de67a7f506e0caa34b36b769d2427194c3362
sheff4uk <sheff4uk@gmail.com>
2023-03-27 08:58:24 +0300

    Отчет по количеству работников на смене

---
+++ cron/shift_report.php
@@ -0,0 +1,73 @@
+<?
+$path = dirname(dirname($argv[0]));
+$key = $argv[1];
+include $path."/config.php";
+// Проверка доступа
+if( $key != $script_key ) die('Access denied!');
+
+// Цикл по производственным участкам
+$query = "
+	SELECT F_ID
+		,notification_group
+	FROM factory
+	WHERE F_ID = 2
+";
+$res = mysqli_query( $mysqli, $query ) or die("Invalid query: " .mysqli_error( $mysqli ));
+while( $row = mysqli_fetch_array($res) ) {
+
+	// Получаем список активных смен
+	$query = "
+		SELECT TS.ts_date
+			,DATE_FORMAT(TS.ts_date, '%d.%m.%Y') ts_date_format
+			,TSS.shift_num
+		FROM TimeReg TR
+		JOIN TimesheetShift TSS ON TSS.TSS_ID = TR.TSS_ID
+			AND TSS.duration IS NULL
+		JOIN Timesheet TS ON TS.TS_ID = TSS.TS_ID
+			AND TS.F_ID = {$row["F_ID"]}
+		WHERE TR.del_time IS NULL
+		GROUP BY TS.ts_date, TSS.shift_num
+		ORDER BY TS.ts_date, TSS.shift_num
+	";
+	$subres = mysqli_query( $mysqli, $query ) or die("Invalid query: " .mysqli_error( $mysqli ));
+	while( $subrow = mysqli_fetch_array($subres) ) {
+
+		// Выводим список работников на смене
+		$query = "
+			SELECT USR_Name(TS.USR_ID) `name`
+				,TR.tr_photo
+				,CONCAT(LPAD((TR.tr_minute DIV 60) % 24, 2, '0'), ':', LPAD(TR.tr_minute % 60, 2, '0')) tr_time
+				,USR.post
+			FROM TimeReg TR
+			JOIN TimesheetShift TSS ON TSS.TSS_ID = TR.TSS_ID
+				AND TSS.duration IS NULL
+				AND TSS.shift_num = {$subrow["shift_num"]}
+			JOIN Timesheet TS ON TS.TS_ID = TSS.TS_ID
+				AND TS.F_ID = {$row["F_ID"]}
+				AND TS.ts_date = '{$subrow["ts_date"]}'
+			JOIN Users USR ON USR.USR_ID = TS.USR_ID
+			WHERE TR.del_time IS NULL
+			ORDER BY `name`
+		";
+		$subsubres = mysqli_query( $mysqli, $query ) or die("Invalid query: " .mysqli_error( $mysqli ));
+		$total_by_post = array();
+		$total = 0;
+		while( $subsubrow = mysqli_fetch_array($subsubres) ) {
+			if( $subsubrow["post"] != null ) {
+				$total_by_post[$subsubrow["post"]]++;
+				$total++;
+		}
+
+		if( $total > 0 ) {
+			$text = "<b>Смена {$subrow["shift_num"]} {$subrow["ts_date_format"]}</b>\n";
+			foreach ($total_by_post as $key => &$value) {
+				$text .= "{$key}: {$value}\n";
+			}
+			$text .= "<b>Всего: {$total}</b>\n";
+			//message_to_telegram($text, $row["notification_group"]);
+			message_to_telegram($text, "217756119");
+		}
+	}
+}
+
+?>
