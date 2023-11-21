690ef3b44a2dd63f396eca11812810ebea20a8a3
sheff4uk <sheff4uk@gmail.com>
2023-11-21 14:39:52 +0300

    test

--- cron/email_production_report.php
+++ cron/email_production_report.php
@@ -170,7 +170,7 @@ $message .= "
 ";
 
 $headers  = "Content-type: text/html; charset=\"utf-8\"\n";
-$headers .= "From: info@konstanta.ltd\r\n";
+$headers .= "From: test@konstanta.ltd\r\n";
 
 mail($to, $subject, $message, $headers);
 // $headers[] = 'MIME-Version: 1.0';
