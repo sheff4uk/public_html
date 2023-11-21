edb98b48c041803e472cf655221202ed328a6b22
sheff4uk <sheff4uk@gmail.com>
2023-11-21 10:43:42 +0300

    test

--- cron/email_shell_report.php
+++ cron/email_shell_report.php
@@ -20,89 +20,90 @@ $message = "
 			<th>Report date: <n style='font-size: 2em;'>{$sr_date_format}</n></th>\n
 		</tr>\n
 	</table>\n
-
-	<table cellspacing='0' cellpadding='2' border='1' style='table-layout: fixed; width: 100%;'>\n
-		<thead style='word-wrap: break-word;'>\n
-			<tr>\n
-				<th>Part-number</th>\n
-				<th>Number of OK shells</th>\n
-				<th>Shell scrap on the past day</th>\n
-				<th>Current need for shells</th>\n
-				<th>Shortage of shells</th>\n
-			</tr>\n
-		</thead>\n
-		<tbody style='text-align: center;'>\n
 ";
+// $message = "
+// 	<table cellspacing='0' cellpadding='2' border='1' style='table-layout: fixed; width: 100%;'>\n
+// 		<thead style='word-wrap: break-word;'>\n
+// 			<tr>\n
+// 				<th>Part-number</th>\n
+// 				<th>Number of OK shells</th>\n
+// 				<th>Shell scrap on the past day</th>\n
+// 				<th>Current need for shells</th>\n
+// 				<th>Shortage of shells</th>\n
+// 			</tr>\n
+// 		</thead>\n
+// 		<tbody style='text-align: center;'>\n
+// ";
 
-$query = "
-	SELECT CW.drawing_item
-		,CW.shell_balance
-		#,ROUND((WB.fillings * PB.in_cassette) / WR.sr_cnt) `durability`
-		#,ROUND(WR.sr_cnt / DATEDIFF(CURDATE() - INTERVAL 1 DAY, '2020-12-04'), 1) `sr_avg`
-		#,ROUND(AVG(PB.fact_batches * PB.fillings / PB.per_batch * PB.in_cassette)) `often`
-		#,MAX(ROUND(PB.fact_batches * PB.fillings / PB.per_batch) * PB.in_cassette) `max`
-		,(
-			SELECT SUM(MixFormula.in_cassette)
-			FROM Cassettes
-			JOIN MixFormula ON MixFormula.F_ID = Cassettes.F_ID
-				AND MixFormula.CW_ID = Cassettes.CW_ID
-			WHERE MixFormula.CW_ID = CW.CW_ID
-		) current_need
-		#,MAX(ROUND(PB.fact_batches * PB.fillings / PB.per_batch) * PB.in_cassette) - CW.shell_balance `need`
-		#,ROUND((CW.shell_balance - MAX(PB.fact_batches * PB.fillings / PB.per_batch * PB.in_cassette)) / (WR.sr_cnt / DATEDIFF(CURDATE() - INTERVAL 1 DAY, '2020-12-04'))) `days_max`
-		#,DATE_FORMAT(CURDATE() + INTERVAL ROUND((CW.shell_balance - MAX(PB.fact_batches * PB.fillings / PB.per_batch * PB.in_cassette)) / (WR.sr_cnt / DATEDIFF(CURDATE() - INTERVAL 1 DAY, '2020-12-04'))) DAY, '%d/%m/%Y') `date_max`
-		,SR.sr_cnt
-	FROM CounterWeight CW
-	LEFT JOIN (
-		SELECT CW_ID
-			,SUM(sr_cnt) sr_cnt
-		FROM shell__Reject
-		WHERE sr_date = CURDATE() - INTERVAL 1 DAY
-		GROUP BY CW_ID
-	) SR ON SR.CW_ID = CW.CW_ID
-	LEFT JOIN plan__Batch PB ON PB.CW_ID = CW.CW_ID
-	# Число заливок с 04.12.2020
-	LEFT JOIN (
-		SELECT PB.CW_ID
-			,SUM(1) fillings
-		FROM list__Filling LF
-		JOIN list__Batch LB ON LB.LB_ID = LF.LB_ID
-		JOIN plan__Batch PB ON PB.PB_ID = LB.PB_ID
-		WHERE DATE(LF.filling_time) BETWEEN '2020-12-04' AND CURDATE() - INTERVAL 1 DAY
-		GROUP BY PB.CW_ID
-	) WB ON WB.CW_ID = CW.CW_ID
-	# Число списаний с 04.12.2020
-	LEFT JOIN (
-		SELECT CW_ID
-			,SUM(sr_cnt) sr_cnt
-		FROM shell__Reject
-		WHERE sr_date BETWEEN '2020-12-04' AND CURDATE() - INTERVAL 1 DAY
-		GROUP BY CW_ID
-	) WR ON WR.CW_ID = CW.CW_ID
-	WHERE CW.CB_ID = {$CB_ID}
-	GROUP BY CW.CW_ID
-	ORDER BY CW.CW_ID
-";
-$res = mysqli_query( $mysqli, $query ) or die("Invalid query: " .mysqli_error( $mysqli ));
-while( $row = mysqli_fetch_array($res) ) {
-	$need = $row["current_need"] - $row["shell_balance"];
-	$message .= "
-		<tr>\n
-			<td>{$row["drawing_item"]}</td>\n
-			<td>{$row["shell_balance"]}</td>\n
-			<td>{$row["sr_cnt"]}</td>\n
-			<td>{$row["current_need"]}</td>\n
-			<td style='color: red;'>".($need > 0 ? $need : "")."</td>\n
-		</tr>\n
-	";
-}
+// $query = "
+// 	SELECT CW.drawing_item
+// 		,CW.shell_balance
+// 		#,ROUND((WB.fillings * PB.in_cassette) / WR.sr_cnt) `durability`
+// 		#,ROUND(WR.sr_cnt / DATEDIFF(CURDATE() - INTERVAL 1 DAY, '2020-12-04'), 1) `sr_avg`
+// 		#,ROUND(AVG(PB.fact_batches * PB.fillings / PB.per_batch * PB.in_cassette)) `often`
+// 		#,MAX(ROUND(PB.fact_batches * PB.fillings / PB.per_batch) * PB.in_cassette) `max`
+// 		,(
+// 			SELECT SUM(MixFormula.in_cassette)
+// 			FROM Cassettes
+// 			JOIN MixFormula ON MixFormula.F_ID = Cassettes.F_ID
+// 				AND MixFormula.CW_ID = Cassettes.CW_ID
+// 			WHERE MixFormula.CW_ID = CW.CW_ID
+// 		) current_need
+// 		#,MAX(ROUND(PB.fact_batches * PB.fillings / PB.per_batch) * PB.in_cassette) - CW.shell_balance `need`
+// 		#,ROUND((CW.shell_balance - MAX(PB.fact_batches * PB.fillings / PB.per_batch * PB.in_cassette)) / (WR.sr_cnt / DATEDIFF(CURDATE() - INTERVAL 1 DAY, '2020-12-04'))) `days_max`
+// 		#,DATE_FORMAT(CURDATE() + INTERVAL ROUND((CW.shell_balance - MAX(PB.fact_batches * PB.fillings / PB.per_batch * PB.in_cassette)) / (WR.sr_cnt / DATEDIFF(CURDATE() - INTERVAL 1 DAY, '2020-12-04'))) DAY, '%d/%m/%Y') `date_max`
+// 		,SR.sr_cnt
+// 	FROM CounterWeight CW
+// 	LEFT JOIN (
+// 		SELECT CW_ID
+// 			,SUM(sr_cnt) sr_cnt
+// 		FROM shell__Reject
+// 		WHERE sr_date = CURDATE() - INTERVAL 1 DAY
+// 		GROUP BY CW_ID
+// 	) SR ON SR.CW_ID = CW.CW_ID
+// 	LEFT JOIN plan__Batch PB ON PB.CW_ID = CW.CW_ID
+// 	# Число заливок с 04.12.2020
+// 	LEFT JOIN (
+// 		SELECT PB.CW_ID
+// 			,SUM(1) fillings
+// 		FROM list__Filling LF
+// 		JOIN list__Batch LB ON LB.LB_ID = LF.LB_ID
+// 		JOIN plan__Batch PB ON PB.PB_ID = LB.PB_ID
+// 		WHERE DATE(LF.filling_time) BETWEEN '2020-12-04' AND CURDATE() - INTERVAL 1 DAY
+// 		GROUP BY PB.CW_ID
+// 	) WB ON WB.CW_ID = CW.CW_ID
+// 	# Число списаний с 04.12.2020
+// 	LEFT JOIN (
+// 		SELECT CW_ID
+// 			,SUM(sr_cnt) sr_cnt
+// 		FROM shell__Reject
+// 		WHERE sr_date BETWEEN '2020-12-04' AND CURDATE() - INTERVAL 1 DAY
+// 		GROUP BY CW_ID
+// 	) WR ON WR.CW_ID = CW.CW_ID
+// 	WHERE CW.CB_ID = {$CB_ID}
+// 	GROUP BY CW.CW_ID
+// 	ORDER BY CW.CW_ID
+// ";
+// $res = mysqli_query( $mysqli, $query ) or die("Invalid query: " .mysqli_error( $mysqli ));
+// while( $row = mysqli_fetch_array($res) ) {
+// 	$need = $row["current_need"] - $row["shell_balance"];
+// 	$message .= "
+// 		<tr>\n
+// 			<td>{$row["drawing_item"]}</td>\n
+// 			<td>{$row["shell_balance"]}</td>\n
+// 			<td>{$row["sr_cnt"]}</td>\n
+// 			<td>{$row["current_need"]}</td>\n
+// 			<td style='color: red;'>".($need > 0 ? $need : "")."</td>\n
+// 		</tr>\n
+// 	";
+// }
 
-$message .= "
-		</tbody>\n
-	</table>\n
-	<br>\n
-	<br>\n
-";
+// $message .= "
+// 		</tbody>\n
+// 	</table>\n
+// 	<br>\n
+// 	<br>\n
+// ";
 
 // $message .= "
 // 	<table cellspacing='0' cellpadding='2' border='1' style='table-layout: fixed; width: 100%;'>
