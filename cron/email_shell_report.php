781bec2075092e3b36a1baaa5a3fde25d574066d
sheff4uk <sheff4uk@gmail.com>
2023-11-21 12:42:30 +0300

    test

--- cron/email_shell_report.php
+++ cron/email_shell_report.php
@@ -13,16 +13,15 @@ $sr_date_format = date_format($date, 'd/m/Y');
 $subject = "[KONSTANTA] Shell/Pallets report on {$sr_date_format}";
 //$subject = "=?utf-8?b?". base64_encode("КОНСТАНТА"). "?=";
 
-$message = "Report date: <n style='font-size: 2em;'></n>";
-// $message = "
-// 	<table cellspacing='0' cellpadding='2' border='1' style='table-layout: fixed; width: 100%;'>\n
-// 		<tr>\n
-// 			<th><img src='https://konstanta.ltd/assets/images/logo.png' alt='KONSTANTA' style='width: 200px; margin: 5px;'></th>\n
-// 			<th><n style='font-size: 2em;'>Shell report</n><br><a href='https://kis.konstanta.ltd/online_shell_report.php'>Click here to open the online report</a></th>\n
-// 			<th>Report date: <n style='font-size: 2em;'>{$sr_date_format}</n></th>\n
-// 		</tr>\n
-// 	</table>\n
-// ";
+$message = "
+	<table cellspacing='0' cellpadding='2' border='1' style='table-layout: fixed; width: 100%;'>\n
+		<tr>\n
+			<th><img src='https://konstanta.ltd/assets/images/logo.png' alt='KONSTANTA' style='width: 200px; margin: 5px;'></th>\n
+			<th><n style='font-size: 2em;'>Shell report</n><br><a href='https://kis.konstanta.ltd/online_shell_report.php'>Click here to open the online report</a></th>\n
+			<th>Report date: <n style='font-size: 2em;'>{$sr_date_format}</n></th>\n
+		</tr>\n
+	</table>\n
+";
 // $message = "
 // 	<table cellspacing='0' cellpadding='2' border='1' style='table-layout: fixed; width: 100%;'>\n
 // 		<thead style='word-wrap: break-word;'>\n
