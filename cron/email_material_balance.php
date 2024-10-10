74b83c1b7c6e9f73d9c1d5ed034bc1121d91dd20
sheff4uk <sheff4uk@gmail.com>
2024-10-10 13:56:07 +0300

    Остатки материалов в днях

--- cron/email_material_balance.php
+++ cron/email_material_balance.php
@@ -40,6 +40,7 @@ $message .= "
 				<th rowspan='2'>Приход</th>
 				<th colspan='2'>Средний расход</th>
 				<th rowspan='2'>Остаток</th>
+                <th rowspan='2'>Запас в днях</th>
 			</tr>
 			<tr>
 				<th>День</th>
@@ -55,6 +56,7 @@ $query = "
         ,MN.material_name
         ,(SELECT ROUND(SUM(ma_cnt), 2) FROM material__Arrival WHERE F_ID = {$F_ID} AND MN_ID = MB.MN_ID AND ma_date = CURDATE()) income
         ,ROUND(MB.mb_balance, 2) mb_balance
+        ,IF(MB.MN_ID = 16, FLOOR(MB.mb_balance * 30 / 1875), IF(MB.MN_ID = 17, FLOOR(MB.mb_balance * 220 / 5000), NULL)) inDays
     FROM material__Balance MB
     JOIN material__Name MN ON MN.MN_ID = MB.MN_ID
     WHERE MB.F_ID = {$F_ID}
@@ -103,6 +105,7 @@ while( $row = mysqli_fetch_array($res) ) {
             <td>{$dayly_avg}</td>
             <td>{$weekly_avg}</td>
             <td>{$row["mb_balance"]}</td>
+            <td>{$row["inDays"]}</td>
         </tr>
     ";
 }
